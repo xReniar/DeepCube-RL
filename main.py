1c5bf82ed1e2615af1c6913f86446766da4785ed
xReniar <ilaorainer@gmail.com>
2025-05-20 00:31:54 +0200

    Add sample code

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import magiccube
+import torch
 
 
 cube = magiccube.Cube(3)
@@ -6,3 +7,9 @@ cube = magiccube.Cube(3)
 
 #print(cube.get_kociemba_facelet_positions())
 #print(cube.get_all_pieces())
+
+cube.rotate("R")
+print(cube)
+
+print(cube.get_kociemba_facelet_colors())
+print(cube.get_kociemba_facelet_positions())
