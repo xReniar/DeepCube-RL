6642d4849870d499d038f604b7cf6ef81b026a13
xReniar <ilaorainer@gmail.com>
2025-06-28 17:36:53 +0200

    Add cube faces tests

--- main.py
+++ main.py
@@ -11,5 +11,15 @@ cube = magiccube.Cube(3)
 cube.rotate("R")
 print(cube)
 
+'''
 print(cube.get_kociemba_facelet_colors())
 print(cube.get_kociemba_facelet_positions())
+'''
+
+positions = cube.get_kociemba_facelet_positions()
+faces = []
+for i in range(0, 6):
+    faces.append(positions[9*i: 9 + 9*i])
+
+
+print(faces)
