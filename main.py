7d955ba6150c5414317c00f843e06ee90001eb53
xReniar <ilaorainer@gmail.com>
2025-07-01 20:57:48 +0200

    .

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
 import magiccube
 import torch
-
-
-cube = magiccube.Cube(3)
+from environment import Cube
 
 
 #print(cube.get_kociemba_facelet_positions())
 #print(cube.get_all_pieces())
-
-cube.rotate("R")
-print(cube)
-
 '''
 print(cube.get_kociemba_facelet_colors())
 print(cube.get_kociemba_facelet_positions())
 '''
 
-positions = cube.get_kociemba_facelet_positions()
-faces = []
-for i in range(0, 6):
-    faces.append(positions[9*i: 9 + 9*i])
-
-
-print(faces)
+env_obj = Cube()
+print(env_obj.graph_state())
