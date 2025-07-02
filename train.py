5ea7c9d733b7ce9c17f5b2a1995a0036ca363d4c
xReniar <ilaorainer@gmail.com>
2025-07-02 17:56:09 +0200

    Add initial train logic

--- train.py
+++ train.py
@@ -9,5 +9,15 @@ print(cube.get_kociemba_facelet_colors())
 print(cube.get_kociemba_facelet_positions())
 '''
 
-env = Environment()
-agent = create_agent("DQN")
+
+if __name__ == "__main__":
+    env = Environment()
+    env.scramble()
+    
+    model = create_agent("DQN")
+    
+    obs = env.reset()
+    done = False
+    while not done:
+        action, _ = model.predict()
+        obs, reward, done = env.step(action)
