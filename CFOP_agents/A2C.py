e3bf44cb95e2c68b6d5e8f3b660434f288f081c4
xReniar <ilaorainer@gmail.com>
2025-07-02 14:36:31 +0200

    Add A2C script implementation

---
+++ CFOP_agents/A2C.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+
+class Actor(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.state_size 
+
+    def forward(self, x: torch.Tensor):
+        pass
