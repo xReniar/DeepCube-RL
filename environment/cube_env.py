6361841c4f0767fbebe4759bf25fd274c0cabe81
xReniar <ilaorainer@gmail.com>
2025-07-02 16:10:55 +0200

    Rename cube to cube_env

--- environment/cube_env.py
+++ environment/cube_env.py
@@ -5,16 +5,21 @@ import torch
 class Environment:
     def __init__(
         self,
-        state: str = None
+        state: str
     ) -> None:
         self.cube = magiccube.Cube(
             size = 3,
             state = state
         )
 
-    def reset(
-        self
-    ) -> tuple:
+        self.start_state: str = self.cube.get_kociemba_facelet_positions()
+        self.state: str = self.start_state
+
+    def reset(self) -> str:
+        self.state = self.start_state
+        return self.state
+    
+    def get_action(self) -> None:
         pass
 
     def step(
