c2e3a37fcb5897fc6e2d0ba92b34d53daee1ba5f
xReniar <ilaorainer@gmail.com>
2025-07-02 18:06:31 +0200

    Add initial layers for DQN

--- CFOP_agents/DQN.py
+++ CFOP_agents/DQN.py
@@ -3,9 +3,16 @@ import torch.nn as nn
 
 
 class DeepQNet(nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        input_dim: int,
+        output_dim: int
+    ) -> None:
         super().__init__()
-        
+        self.layer1 = nn.Linear(input_dim, output_dim * 2)
+        self.layer2 = nn.Linear(output_dim * 2, output_dim)
 
     def forward(self, x: torch.Tensor):
+        x = self.layer1(x)
+        x = self.layer2(x)
         return x
