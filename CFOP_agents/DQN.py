4f98a136da47d64b75d262f6c0f8e4266f3c4bb8
xReniar <ilaorainer@gmail.com>
2025-07-03 00:21:48 +0200

    Add two layers

--- CFOP_agents/DQN.py
+++ CFOP_agents/DQN.py
@@ -1,18 +1,24 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class DeepQNet(nn.Module):
     def __init__(
         self,
         input_dim: int,
+        hidden_dim: int,
         output_dim: int
     ) -> None:
         super().__init__()
-        self.layer1 = nn.Linear(input_dim, output_dim * 2)
-        self.layer2 = nn.Linear(output_dim * 2, output_dim)
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, hidden_dim // 2)
+        self.fc4 = nn.Linear(hidden_dim // 2, output_dim)
 
     def forward(self, x: torch.Tensor):
-        x = self.layer1(x)
-        x = self.layer2(x)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         return x
