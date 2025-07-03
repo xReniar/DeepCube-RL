72a777b32349a57fbb20af0856f7d324b2cd7c22
xReniar <ilaorainer@gmail.com>
2025-07-03 16:48:50 +0200

    Add store_transition and chooseAction functions

--- CFOP_agents/DQN.py
+++ CFOP_agents/DQN.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch import optim
 from .agent import Agent
+import numpy as np
 
 
 class DeepQNet(nn.Module):
@@ -9,7 +11,8 @@ class DeepQNet(nn.Module):
         self,
         input_dim: int,
         hidden_dim: int,
-        output_dim: int
+        output_dim: int,
+        alpha: float
     ) -> None:
         super().__init__()
         self.fc1 = nn.Linear(input_dim, hidden_dim)
@@ -28,6 +31,40 @@ class DeepQNet(nn.Module):
 class DQN(Agent):
     def __init__(self, args):
         super().__init__(args)
+        self.GAMMA = args["gamma"]
+        self.EPSILON = args["epsilon"]
+        self.EPS_END = args["eps_end"]
+        self.action_space = args["actionSpace"]
+        self.memSize = args["maxMemorySize"]
+        self.steps = 0
+        self.learn_step_counter = 0
+        self.memory = []
+        self.memCntr = 0
+        self.replace_target_cnt = args["replace"]
+        self.Q_eval = DeepQNet(54, 128, 12, )
+        self.T_eval = DeepQNet(54, 128, 12)
+
+    def store_transition(self, state, action, reward, state_):
+        if self.memCntr < self.memSize:
+            self.memory.append([state, action, reward, state_])
+        else:
+            self.memory[self.memCntr % self.memSize] = [state, action, reward, state_]
+        self.memCntr += 1
+
+    def chooseAction(self, state: str):
+        rand = np.random.random()
+        actions = self.Q_eval(state)
+
+        if rand < 1 - self.EPSILON:
+            action = torch.argmax(actions[1]).item()
+        else:
+            action = np.random.choice(self.action_space)
+        self.steps += 1
+        
+        return action
+    
+    def learn(self, batch_size):
+        pass
 
     def predict(self, state: str):
         pass
