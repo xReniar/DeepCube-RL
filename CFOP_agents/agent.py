a7f7e802f62de99334d3f0f32ddf7830f5042598
xReniar <ilaorainer@gmail.com>
2025-07-04 14:13:52 +0200

    Add device for training ad rename predict to action

--- CFOP_agents/agent.py
+++ CFOP_agents/agent.py
@@ -1,11 +1,17 @@
 from abc import ABC, abstractmethod
+import torch
 
 
 class Agent(ABC):
     def __init__(self, args: dict):
         super().__init__()
         self.args = args
+        self.device = torch.device(
+            "cuda" if torch.cuda.is_available() else
+            "mps" if torch.backends.mps.is_available() else
+            "cpu"
+        )
 
     @abstractmethod
-    def predict(self, state: str):
+    def action(self, state: str):
         pass
