dc25a379db1701ebebe93d089ab77a5404484652
xReniar <ilaorainer@gmail.com>
2025-07-04 23:11:11 +0200

    Remove init_agent function

--- CFOP_agents/__init__.py
+++ CFOP_agents/__init__.py
@@ -1,18 +1,2 @@
 from .A2C import A2C
 from .DQN import DQN
-from .agent import Agent
-
-
-def init_agent(agent_type: str, args: dict) -> Agent:
-    if agent_type == "A2C":
-        return A2C(args)
-    elif agent_type == "DQN":
-        return DQN(args)
-    else:
-        raise ValueError(f"No {agent_type} agent type exists!!!")
-
-
-__all__ = [
-    "init_agent",
-    "Agent"
-]
