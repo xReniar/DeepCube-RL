ac50dc88d96209b7a5cd5374b9a25948deaac757
xReniar <ilaorainer@gmail.com>
2025-07-04 23:11:21 +0200

    Add convert_state_to_tensor

--- CFOP_agents/agent.py
+++ CFOP_agents/agent.py
@@ -12,6 +12,17 @@ class Agent(ABC):
             "cpu"
         )
 
+    def convert_state_to_tensor(self, state: str) -> torch.Tensor:
+        colors = ['U', 'D', 'F', 'R', 'B', 'L']
+        color_to_idx = {color: idx for idx, color in enumerate(colors)}
+        num_colors = len(colors)
+
+        one_hot = torch.zeros(54, num_colors, dtype=torch.float32)
+        for i, color in enumerate(state):
+            one_hot[i, color_to_idx[color]] = 1.0
+
+        return one_hot.flatten()
+    
     @abstractmethod
     def action(self, state: str):
         pass
