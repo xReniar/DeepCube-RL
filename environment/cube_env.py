0d277a46560626394e5b712bfb13cc9a5f4b6151
xReniar <ilaorainer@gmail.com>
2025-07-05 16:11:27 +0200

    Fix is_terminated

--- environment/cube_env.py
+++ environment/cube_env.py
@@ -1,27 +1,37 @@
 from .algorithm import Algorithm, init_algo
 from magiccube import Cube
 import random
+import torch
 
 
-moves = ['R', "R'", 'L', "L'", 'U', "U'", 'D', "D'", 'F', "F'", 'B', "B'"]
+moves = ["U", "D", "F", "R", "B", "L", "U'", "D'", "F'", "R'", "B'", "L'"]
 
 class Environment:
     def __init__(
         self,
         method: str,
-        state: str = None
+        size: int,
+        device: str
     ) -> None:
-        self.cube = Cube(
-            size = 3,
-            state = state
-        )
+        self.cube = Cube(size=size)
         self.algorithm: Algorithm = init_algo(method)
+        self.device = device
         
         self.scramble() # to start with a scrambled cube
-        self.start_state: str = self.cube.get_kociemba_facelet_positions()
-        self.state: str = self.start_state
+        self.start_state: torch.Tensor = self.__state_to_tensor(self.cube.get_kociemba_facelet_positions())
+        self.state: torch.Tensor = self.start_state
 
-    def reset(self) -> str:
+    def __state_to_tensor(self, state: str) -> torch.Tensor:
+        state_for_tensor = []
+        for s in state:
+            state_for_tensor.append(state[s])
+
+        return torch.tensor(state_for_tensor, device=self.device, dtype=torch.float)
+    
+    def __convert_tensor_to_move(self, tensor_action: torch.Tensor) -> str:
+        return moves[tensor_action.item()]
+
+    def reset(self) -> torch.Tensor:
         '''
         Reset the environment to get the first observation
         '''
@@ -37,12 +47,12 @@ class Environment:
         for i in range(0, 6):
             faces.append(positions[9*i: 9 + 9*i])
 
-        top = any([face == "U" for face in faces[0]])
-        right = any([face == "R" for face in faces[1]])
-        front = any([face == "F" for face in faces[2]])
-        bottom = any([face == "D" for face in faces[3]])
-        left = any([face == "L" for face in faces[4]])
-        back = any([face == "B" for face in faces[5]])
+        top = all([face == "U" for face in faces[0]])
+        right = all([face == "R" for face in faces[1]])
+        front = all([face == "F" for face in faces[2]])
+        bottom = all([face == "D" for face in faces[3]])
+        left = all([face == "L" for face in faces[4]])
+        back = all([face == "B" for face in faces[5]])
 
         return top and right and front and bottom and left and back
     
@@ -51,15 +61,16 @@ class Environment:
         Scrambles the cube
         '''
         self.cube.rotate(' '.join(random.choices(moves, k=20)))
-        self.state = self.cube.get_kociemba_facelet_positions()
+        self.state = self.__state_to_tensor(self.cube.get_kociemba_facelet_positions())
 
     def step(
         self,
-        action: str
+        action_tensor: torch.Tensor
     ) -> tuple:
         # makes action and update state
-        self.cube.rotate(action)
-        self.state = self.cube.get_kociemba_facelet_positions()
+        move = self.__convert_tensor_to_move(action_tensor)
+        self.cube.rotate(move)
+        self.state = self.__state_to_tensor(self.cube.get_kociemba_facelet_positions())
 
         # calculate reward
         reward = self.algorithm.status(self.cube)
