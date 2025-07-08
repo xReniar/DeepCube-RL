79f4f7afc5947d389e6ebbf088512fc716657b16
xReniar <ilaorainer@gmail.com>
2025-07-08 13:17:49 +0200

    Add network

--- CFOP_agents/PPO.py
+++ CFOP_agents/PPO.py
@@ -1,10 +1,25 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 from .agent import Agent
+
+
+class Network(nn.Module):
+    def __init__(
+        self,
+        input_dim: int,
+        hidden_dim: int,
+        output_dim: int
+    ) -> None:
+        super().__init__()
+
+        pass
+
+    def forward(self, x: torch.Tensor):
+        pass
 
 
 class PPO(Agent):
     def __init__(self, args):
         super().__init__(args)
-
