61ee3dc7dd7515e3648c4f57859ee04155861178
xReniar <ilaorainer@gmail.com>
2025-07-08 13:17:33 +0200

    Separate training logic

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 from environment import Environment
-from CFOP_agents import DQN
+from CFOP_agents import A2C, DQN, PPO
 import yaml
 from itertools import count
 import torch
@@ -11,23 +11,13 @@ device = torch.device(
     "cpu"
 )
 
-if __name__ == "__main__":
-    env = Environment(
-        method="CFOP",
-        size=3,
-        device=device
-    )
-    env.scramble()
-    
-    args = yaml.safe_load(open("config.yaml", "r"))
-    agent: DQN = DQN(args["DQN"])
-
+def train_DQN(env: Environment, agent: DQN):
     for episode in range(agent.num_episodes):
         state = env.reset()
 
         current_reward = env.algorithm.status(env.cube)
-
-        for t in count():
+        print(episode)
+        for t in range(env.scramble_moves):
             action = agent.action(state)
             obs, reward, done = env.step(action.item())
 
@@ -52,6 +42,27 @@ if __name__ == "__main__":
             agent.target_net.load_state_dict(target_net_state_dict)
 
             if done:
-                with open(f"episode{episode}-{t}.txt", "w") as f:
+                with open(f"log/episode{episode}-{t}.txt", "w") as f:
                     f.write(env.cube.__str__())
+                    f.write(env.start_state)
                 break
+
+def train_A2C(env: Environment, agent: A2C):
+    t_so_far = 0
+    i_so_far = 0
+
+    while t_so_far < agent.total_timesteps:
+        batch_obs, batch_acts, batch_log_probs, batch_rtgs, batch_lens = agent.rollout()
+
+if __name__ == "__main__":
+    args = yaml.safe_load(open("config.yaml", "r"))
+
+    env = Environment(
+        method="CFOP",
+        size=3,
+        device=device,
+        args=args
+    )
+
+    train_DQN(env, DQN(args["DQN"]))
+    #train_A2C(env, A2C(args["A2C"]))
