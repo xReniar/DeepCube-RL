118e5d09cda844265e3d9a4018a9fd1de1df60a2
xReniar <ilaorainer@gmail.com>
2025-07-10 15:15:54 +0200

    Remove state to tensor function

--- CFOP_agents/agent.py
+++ CFOP_agents/agent.py
@@ -8,21 +8,10 @@ class Agent(ABC):
         self.args = args
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else
-            "mps" if torch.backends.mps.is_available() else
+            "mps" if torch.mps.is_available() else
             "cpu"
         )
-
-    def convert_state_to_tensor(self, state: str) -> torch.Tensor:
-        colors = ['U', 'D', 'F', 'R', 'B', 'L']
-        color_to_idx = {color: idx for idx, color in enumerate(colors)}
-        num_colors = len(colors)
-
-        one_hot = torch.zeros(54, num_colors, dtype=torch.float32)
-        for i, color in enumerate(state):
-            one_hot[i, color_to_idx[color]] = 1.0
-
-        return one_hot.flatten().to(self.device)
     
     @abstractmethod
-    def action(self, state: str):
+    def action(self, state: torch.Tensor):
         pass
