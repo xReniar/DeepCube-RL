1af56970a0475a97897144f655c30329ef033d91
xReniar <ilaorainer@gmail.com>
2025-07-24 06:48:46 +0200

    Implement move click

--- data/create_data.js
+++ data/create_data.js
@@ -1,5 +1,31 @@
 import { launch } from "puppeteer";
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function executeRubikMove(page, scramble) {
+    const moveMappings = {
+        // Clockwise
+        'U': 'uu', 'L': 'll', 'R': 'rr',
+        'D': 'dd', 'F': 'ff', 'B': 'bb',
+        // Counter Clockwise
+        "U'": 'ui', "L'": 'li', "R'": 'ri',
+        "D'": 'di', "F'": 'fi', "B'": 'bi',
+    };
+
+    const moves = scramble.trim().split(/\s+/);
+    for (const move of moves) {
+        const functionName = moveMappings[move];
+        await page.evaluate((fnName) => {
+            if (typeof window[fnName] === 'function') {
+                window[fnName]();
+                kiir();
+            }
+        }, functionName);
+        sleep(100);
+    }
+}
 
 const moves = ['R', "R'", 'L', "L'", 'U', "U'", 'D', "D'", 'F', "F'", 'B', "B'"];
 const dataset = new Set();
@@ -21,8 +47,14 @@ const browser = await launch({
 });
 
 const page = await browser.newPage();
-page.goto("https://solverubikscube.com/", { waitUntil: 'domcontentloaded' }).then((value) => {
-    
-}).catch((reason) => {
-    console.log(reason);
-});
+await page.goto("https://solverubikscube.com/", { waitUntil: 'domcontentloaded' })
+    .then(async () => {
+        //console.log(scramble);
+        for (const scramble of dataset) {
+            console.log(scramble);
+            await executeRubikMove(page, scramble);
+            await sleep(1000);
+        }
+    }).catch((reason) => {
+        console.log(reason);
+    });
