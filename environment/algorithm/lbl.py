080861205f866db1e40a15d5d2a39d15709bcd61
xReniar <ilaorainer@gmail.com>
2025-07-24 22:04:31 +0200

    Update reward function for cross and first 2 layers

--- environment/algorithm/lbl.py
+++ environment/algorithm/lbl.py
@@ -20,7 +20,11 @@ class LBL(Algorithm):
         piece_3 = (faces["back"][7] == faces["back"][4]) and (faces["bottom"][7] == faces["bottom"][4])
         piece_4 = (faces["left"][7] == faces["left"][4]) and (faces["bottom"][3] == faces["bottom"][4])
 
-        return piece_1 + piece_2 + piece_3 + piece_4
+        sum_reward = piece_1 + piece_2 + piece_3 + piece_4
+        if sum_reward == 4:
+            return sum_reward * 10
+        else:
+            return sum_reward
 
     def first_layer(self, cube: Cube) -> int:
         faces = self.cube_faces(cube)
@@ -30,7 +34,11 @@ class LBL(Algorithm):
         piece_3 = (faces["back"][8] == faces["back"][4]) and (faces["left"][6] == faces["left"][4])
         piece_4 = (faces["left"][8] == faces["left"][4]) and (faces["front"][6] == faces["front"][4])
 
-        return piece_1 + piece_2 + piece_3 + piece_4
+        sum_reward = piece_1 + piece_2 + piece_3 + piece_4
+        if sum_reward == 4:
+            return sum_reward * 10
+        else:
+            return sum_reward
 
     def second_layer(self, cube: Cube) -> int:
         faces = self.cube_faces(cube)
@@ -40,7 +48,11 @@ class LBL(Algorithm):
         piece_3 = (faces["back"][5] == faces["back"][4]) and (faces["left"][3] == faces["left"][4])
         piece_4 = (faces["left"][5] == faces["left"][4]) and (faces["front"][3] == faces["front"][4])
 
-        return piece_1 + piece_2 + piece_3 + piece_4
+        sum_reward = piece_1 + piece_2 + piece_3 + piece_4
+        if sum_reward == 4:
+            return sum_reward * 10
+        else:
+            return sum_reward
 
     def top_cross(self, cube: Cube) -> int:
         faces = self.cube_faces(cube)
