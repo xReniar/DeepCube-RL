15c5e04f526e8e95de5deff214e7d5ee5f4d8e89
xReniar <ilaorainer@gmail.com>
2025-07-25 22:04:08 +0200

    Read data and simulating cube

--- data/format_data.py
+++ data/format_data.py
@@ -1,4 +1,24 @@
+from magiccube import Cube
 import json
+import time
 
 
 data = json.load(open("data.json", "r"))
+
+for element in data.keys():
+    cube = Cube()
+
+    instance = data[element]
+    scramble:str = instance["scramble"]
+    solution:str = instance["solution"]
+
+    cube.rotate(scramble)
+
+    print(cube)
+    time.sleep(1)
+    
+    formatted_solution = {}
+    
+    for rotation in solution.split():
+        print(cube.get_kociemba_facelet_positions())
+        cube.rotate(rotation)
