6b0c7c1156e5a4c101538d27852fb320a155a3f6
xReniar <ilaorainer@gmail.com>
2025-07-27 02:00:58 +0200

    Fix graph_neighbors

--- CFOP_agents/nnet/graph_neighbors.py
+++ CFOP_agents/nnet/graph_neighbors.py
@@ -1,185 +1,75 @@
 import networkx as nx
-import matplotlib.pyplot as plt
 from magiccube import Cube
-import hashlib
 from collections import deque
 
-
-def hash5(s: str) -> int:
-    digits = 5
-    h = hashlib.sha256(s.encode()).hexdigest()
-    return int(h, 16) % (pow(10, digits))
-
-def draw_labeled_multigraph_tree(G, attr_name="m", ax=None, show_labels=False):
-    if ax is None:
-        ax = plt.gca()
-
-    root = next(iter(G.nodes))
-
-    levels = {}
-    queue = deque([(root, 0)])
-    visited = set()
-
-    while queue:
-        node, depth = queue.popleft()
-        if node in visited:
-            continue
-        visited.add(node)
-        levels[node] = depth
-        for neighbor in G.successors(node):
-            if neighbor not in visited:
-                queue.append((neighbor, depth + 1))
-
-    pos = {}
-    nodes_by_level = {}
-    for node, level in levels.items():
-        nodes_by_level.setdefault(level, []).append(node)
-
-    for level, nodes in nodes_by_level.items():
-        for i, node in enumerate(nodes):
-            pos[node] = (i - len(nodes) / 2, -level)
-
-    nx.draw_networkx_nodes(G, pos, ax=ax, node_size=700)
-    nx.draw_networkx_labels(G, pos, font_size=10, ax=ax)
-
-    nx.draw_networkx_edges(
-        G,
-        pos,
-        edge_color="grey",
-        arrows=True,
-        arrowstyle="-|>",
-        arrowsize=20,
-        ax=ax,
-    )
-
-    if show_labels:
-        labels = {
-            tuple(edge): f"{attr_name}={attrs[attr_name]}"
-            for *edge, attrs in G.edges(keys=True, data=True)
-        }
-
-        nx.draw_networkx_edge_labels(
-            G,
-            pos,
-            labels,
-            label_pos=0.3,
-            font_color="blue",
-            bbox={"alpha": 0},
-            ax=ax,
-        )
-
-def draw_labeled_multigraph_tree2(G, attr_name="m", ax=None, show_labels=False):
-    if ax is None:
-        ax = plt.gca()
-
-    root = next(iter(G.nodes))
-
-    levels = {}
-    queue = deque([(root, 0)])
-    visited = set()
-
-    while queue:
-        node, depth = queue.popleft()
-        if node in visited:
-            continue
-        visited.add(node)
-        levels[node] = depth
-        for neighbor in G.successors(node):
-            if neighbor not in visited:
-                queue.append((neighbor, depth + 1))
-
-    pos = {}
-    nodes_by_level = {}
-    for node, level in levels.items():
-        nodes_by_level.setdefault(level, []).append(node)
-
-    for level, nodes in nodes_by_level.items():
-        for i, node in enumerate(nodes):
-            pos[node] = (i - len(nodes) / 2, -level)
-
-    nx.draw_networkx_nodes(G, pos, ax=ax, node_size=700)
-    nx.draw_networkx_labels(G, pos, font_size=10, ax=ax)
-
-    nx.draw_networkx_edges(
-        G,
-        pos,
-        edge_color="grey",
-        arrows=True,
-        arrowstyle="-|>",
-        arrowsize=20,  # <-- frecce più grandi
-        ax=ax,
-    )
-
-    if show_labels:
-        labels = {
-            (u, v): f"{attr_name}={attrs[attr_name]}"
-            for u, v, attrs in G.edges(data=True)
-        }
-
-        nx.draw_networkx_edge_labels(
-            G,
-            pos,
-            labels,
-            label_pos=0.3,
-            font_color="blue",
-            bbox={"alpha": 0},
-            ax=ax,
-        )
-
-
-def generate_state_graph(cube: Cube, moves: list, depth: int):
-    G = nx.DiGraph()
-    visited = set()
-
-    start_k_state = cube.get_kociemba_facelet_colors()
-    start_hash = hash5(start_k_state)
-    
-    queue = deque()
-    queue.append((start_hash, start_k_state, 0))
-
-    visited.add(start_hash)
-
-    while queue:
-        current_hash, current_k_state, d = queue.popleft()
-
-        if d >= depth:
-            continue
-
-        for move in moves:
-            child_cube = Cube(state=current_k_state)
-            child_cube.rotate(move)
-            new_k_state = child_cube.get_kociemba_facelet_colors()
-            new_hash = hash5(new_k_state)
-
-            G.add_edge(current_hash, new_hash, m=move)
-
-            inverse_move = move[:-1] if "'" in move else f"{move}'"
-            G.add_edge(new_hash, current_hash, m=inverse_move)
-
-            if new_hash not in visited:
-                visited.add(new_hash)
-                queue.append((new_hash, new_k_state, d + 1))
-
-    return G
 
 moves = ["U", "D", "F", "R", "B", "L",
          "U'", "D'", "F'", "R'", "B'", "L'"]
 
-G = generate_state_graph(cube=Cube(), moves=moves, depth=1)
+
+def convert_to_color(kociemba_state: str) -> str:
+    state = list(kociemba_state)
+    new_state = []
+
+    kociemba_to_color = {
+        "U": "W",
+        "D": "Y",
+        "F": "G",
+        "R": "R",
+        "B": "B",
+        "L": "O"
+    }
+
+    for x in state:
+        new_state.append(kociemba_to_color[x])
+
+    new_state = "".join(new_state)
+    faces = [new_state[i:i+9] for i in range(0, 54, 9)]
+
+    return f"{faces[0]}{faces[4]}{faces[2]}{faces[1]}{faces[5]}{faces[3]}"
 
 
-import time
-for node in list(G.nodes()):
-    succ = len(list(G.successors(node)))
-    prec = len(list(G.predecessors(node)))
+def _neighbors(
+    cube: Cube
+) -> nx.DiGraph:
+    global moves
+    G = nx.DiGraph()
 
-    if not(succ == 1 and prec == succ):
-        print(node, succ, prec)
+    for move in moves:
+        current_k_state = cube.get_kociemba_facelet_positions()
+        cube.rotate(move)
+        new_k_state = cube.get_kociemba_facelet_positions()
+        inverse_move = move[:1] if "'" in move else f"{move}'"
 
-'''
-fig, ax = plt.subplots(figsize=(6, 5))
-draw_labeled_multigraph_tree2(G, "m", ax, show_labels=True)
-ax.set_title("Graph")
-plt.tight_layout()
-plt.show()
-'''
+        G.add_edge(current_k_state, new_k_state, m=move)
+        #G.add_edge(new_k_state, current_k_state, m=inverse_move)
+        
+        cube.rotate(inverse_move)
+
+    return G
+
+def generate_neighbors(cube: Cube, depth: int):
+    G = nx.DiGraph()
+    visited = set()
+
+    start_k_state = cube.get_kociemba_facelet_positions()
+
+    queue = deque()
+    queue.append((start_k_state, 0))
+    visited.add(start_k_state)
+
+    while queue:
+        current_k_state, d = queue.popleft()
+
+        if d >= depth:
+            continue
+
+        neighbors = _neighbors(cube = Cube(state=convert_to_color(current_k_state)))
+
+        for node in neighbors.nodes():
+            if node not in visited:
+                visited.add(node)
+                queue.append((node, d + 1))
+        G = nx.compose(G, neighbors)
+
+    return G
