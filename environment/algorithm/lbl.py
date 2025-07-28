45dbac40e5cd200831bc5cadb8733205473709b8
xReniar <ilaorainer@gmail.com>
2025-07-28 14:54:03 +0200

    Improve cross reward function. Update status function

--- environment/algorithm/lbl.py
+++ environment/algorithm/lbl.py
@@ -3,28 +3,42 @@ from magiccube import Cube
 
 
 class LBL(Algorithm):
-    def __init__(
-        self,
-        cube: Cube
-    ) -> None:
-        self.cube = cube
+    def __init__(self) -> None:
+        super().__init__()
 
     def bottom_cross(self, cube: Cube) -> int:
         '''
         Checks if bottom cross pieces are inserted correctly
         '''
         faces = self.cube_faces(cube)
-        
-        piece_1 = (faces["front"][7] == faces["front"][4]) and (faces["bottom"][1] == faces["bottom"][4])
-        piece_2 = (faces["right"][7] == faces["right"][4]) and (faces["bottom"][5] == faces["bottom"][4])
-        piece_3 = (faces["back"][7] == faces["back"][4]) and (faces["bottom"][7] == faces["bottom"][4])
-        piece_4 = (faces["left"][7] == faces["left"][4]) and (faces["bottom"][3] == faces["bottom"][4])
 
-        sum_reward = piece_1 + piece_2 + piece_3 + piece_4
-        if sum_reward == 4:
-            return sum_reward * 10
-        else:
-            return sum_reward
+        sides = ["front", "right", "back", "left"]
+        adjacency = {}
+        for i, direction in enumerate(sides):
+            left_adj = sides[(i - 1) % len(sides)]
+            right_adj = sides[(i + 1) % len(sides)]
+            adjacency[direction] = [left_adj, right_adj]
+
+        cross_status = 0.0
+        for face in adjacency.keys():
+            if ((faces[face][3] == faces[face][4]) and (faces[adjacency[face][0]][5] == faces["bottom"][4])) or \
+               ((faces[face][5] == faces[face][4]) and (faces[adjacency[face][1]][3] == faces["bottom"][4])):
+                cross_status += 2
+            else:
+                if face == "front":
+                    cross_status += int((faces[face][1] == faces[face][4]) and (faces["top"][7] == faces["bottom"][4])) * 1 + \
+                                    int((faces[face][7] == faces[face][4]) and (faces["bottom"][1] == faces["bottom"][4])) * 3
+                elif face == "right":
+                    cross_status += int((faces[face][1] == faces[face][4]) and (faces["top"][5] == faces["bottom"][4])) * 1 + \
+                                    int((faces[face][7] == faces[face][4]) and (faces["bottom"][5] == faces["bottom"][4])) * 3
+                elif face == "back":
+                    cross_status += int((faces[face][1] == faces[face][4]) and (faces["top"][1] == faces["bottom"][4])) * 1 + \
+                                    int((faces[face][7] == faces[face][4]) and (faces["bottom"][7] == faces["bottom"][4])) * 3
+                else:
+                    cross_status += int((faces[face][1] == faces[face][4]) and (faces["top"][3] == faces["bottom"][4])) * 1 + \
+                                    int((faces[face][7] == faces[face][4]) and (faces["bottom"][3] == faces["bottom"][4])) * 3
+
+        return cross_status
 
     def first_layer(self, cube: Cube) -> int:
         faces = self.cube_faces(cube)
@@ -64,9 +78,4 @@ class LBL(Algorithm):
         faces = self.cube_faces(cube)
     
     def status(self, cube: Cube) -> int:
-        return self.bottom_cross(cube) \
-            + self.first_layer(cube) \
-            + self.second_layer(cube) \
-            + self.top_cross(cube) \
-            + self.top_edge(cube) \
-            + self.top_corners(cube)
+        return self.bottom_cross(cube)
