9f597e5cf8d1d0c0dd5b5d0616200735c28fa2ed
xReniar <ilaorainer@gmail.com>
2025-08-24 14:04:33 +0200

    Add learn and rollout functions with iperparameters initialization

--- agents/PPO.py
+++ agents/PPO.py
@@ -14,12 +14,59 @@ class Network(nn.Module):
     ) -> None:
         super().__init__()
 
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, output_dim)
         pass
 
     def forward(self, x: torch.Tensor):
-        pass
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
+        
 
 
 class PPO(Agent):
-    def __init__(self, args):
+    def __init__(self, args, env):
         super().__init__(args)
+        self.total_timesteps = int(args["total_timesteps"])
+        self.timesteps_per_batch = int(args["timesteps_per_batch"])
+        self.max_timesteps_per_episode = int(args["max_timesteps_per_episode"])
+
+        # environment info
+        self.env = env
+        self.obs_dim = 54
+        self.act_dim = 12
+
+        # initialize actor and critic networks
+        self.actor = Network(self.obs_dim, 64, self.act_dim)
+        self.critic = Network(self.obs_dim, 1)
+
+        # covariance matrice for get_action
+        self.cov_var = torch.full(size=(self.act_dim,), fill_value=0.5)
+        self.cov_mat = torch.diag(self.cov_var)
+
+    def learn(self) -> None:
+        t = 0
+
+        while t < self.total_timesteps:
+            ep_rews = []
+
+            obs = self.env.reset()
+            done = False
+
+            for ep_t in range(self.max_timesteps_per_episode):
+                t += 1
+                batch_obs.append()
+
+    def rollout(self):
+        # Batch data
+        batch_obs = []
+        batch_acts = []
+        batch_log_probs = []
+        batch_rews = []
+        batch_rtgs = []
+        batch_lens = []
+
