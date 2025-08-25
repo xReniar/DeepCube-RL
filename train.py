3f4a70d390b1083e16ba0c5cef1fa388fadbc17d
xReniar <ilaorainer@gmail.com>
2025-08-25 23:33:21 +0200

    Update train.py

--- train.py
+++ train.py
@@ -1,15 +1,7 @@
 from environment import Environment
 from agents import A2C, DQN, PPO
 import yaml
-from itertools import count
-import torch
 
-
-device = torch.device(
-    "cuda" if torch.cuda.is_available() else
-    "mps" if torch.mps.is_available() else
-    "cpu"
-)
 
 if __name__ == "__main__":
     args = yaml.safe_load(open("config.yaml", "r"))
@@ -17,7 +9,6 @@ if __name__ == "__main__":
     env = Environment(
         method="LBL",
         size=3,
-        device=device,
         args=args["environment"]
     )
 
