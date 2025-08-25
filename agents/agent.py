dd1c5cadc7df91cec6c4cd98be2ea360e6877c10
xReniar <ilaorainer@gmail.com>
2025-08-25 23:32:36 +0200

    Add utils function to convert action and state to tensor format

--- agents/agent.py
+++ agents/agent.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from environment import Environment
 import torch
+import numpy as np
 
 
 class Agent(ABC):
@@ -17,6 +18,15 @@ class Agent(ABC):
             "mps" if torch.mps.is_available() else
             "cpu"
         )
+
+    def action_to_move(self, move_id: int) -> str:
+        mapper = {i: action.item() for i, action in enumerate(self.env.action_space)}
+        return mapper[move_id]
+    
+    def state_to_tensor(self, state: np.ndarray) -> torch.Tensor:
+        mapper = {action: i for i, action in enumerate(["U", "D", "F", "R", "B", "L"])}
+        tensor_state = torch.from_numpy(np.vectorize(mapper.get)(state)).to(self.device).float()
+        return tensor_state.unsqueeze(0)
     
     @abstractmethod
     def action(self, state: torch.Tensor):
