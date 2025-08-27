a09fe5138a0e1b8bd5f21b7265c24020449a5a46
xReniar <ilaorainer@gmail.com>
2025-08-27 18:58:10 +0200

    Remove plot_durations function

--- agents/DQN.py
+++ agents/DQN.py
@@ -9,15 +9,7 @@ from environment import Environment
 import random
 import numpy as np
 import math
-import matplotlib
 import matplotlib.pyplot as plt
-
-
-is_ipython = 'inline' in matplotlib.get_backend()
-if is_ipython:
-    from IPython import display
-
-plt.ion()
 
 
 class DeepQNet(nn.Module):
@@ -179,29 +171,3 @@ class DQN(Agent):
                     break
                 
         # save model
-
-
-    def plot_durations(self, show_result=False):
-        durations_t = torch.tensor(self.episode_durations, dtype=torch.float)
-        if show_result:
-            plt.title('Result')
-        else:
-            plt.clf()
-            plt.title('Training...')
-        plt.xlabel('Episode')
-        plt.ylabel('Duration')
-        plt.plot(durations_t.numpy())
-        # Take 100 episode averages and plot them too
-        if len(durations_t) >= 100:
-            means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
-            means = torch.cat((torch.zeros(99), means))
-            plt.plot(means.numpy())
-
-        plt.pause(0.001)  # pause a bit so that plots are updated
-        if is_ipython:
-            if not show_result:
-                display.display(plt.gcf())
-                display.clear_output(wait=True)
-            else:
-                display.display(plt.gcf())
-        print("ciao")
