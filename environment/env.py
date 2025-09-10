4cb03e45b83551f21c59e3244a9ecd8b66661f7f
xReniar <ilaorainer@gmail.com>
2025-09-10 12:06:48 +0200

    Rename cube_env to env

--- environment/env.py
+++ environment/env.py
@@ -1,24 +1,49 @@
 from .algorithm import Algorithm, init_algo
 from magiccube import Cube
 from .dummy_cube import DummyCube
+from enum import Enum
 import itertools
 import numpy as np
 import random
 
 
+class Phase(Enum):
+    CROSS = "cross"
+    F2L = "f2l"
+    OLL = "oll"
+    PLL = "pll"
+
 class Environment:
     def __init__(
         self,
         method: str,
+        phase: Phase,
         size: int,
         args: dict
     ) -> None:
+        self.phase = phase
         self._scramble_moves = int(args["scramble_moves"])
         self.cube = Cube(size=size)
         self.algorithm: Algorithm = init_algo(method, self.cube)
 
+        if self.phase == Phase.CROSS:
+            self.action_space = np.array(["U", "D", "F", "R", "B", "L","U'", "D'", "F'", "R'", "B'", "L'"])
+        elif self.phase == Phase.F2L:
+            self.action_space = np.array([
+                "L' U L", "R U' R'", "F' U F", "B U' B'",                   # insert moves good orientation case 1 (first layer)
+                "R' U R", "L U' L'", "B' U B", "F U' F'",                   # insert moves good orientation case 2 (first layer)
+                "F' U2 F U2", "R' U2 R U2", "B' U2 B U2", "L' U2 L U2",     # orient moves bad orientation (first layer)
+                "R U' R' U'", "F' U F U", "B U' B' U'", "R' U R U",         # insert moves (second layer) 
+                "L U' L' U'", "B' U B U", "F U' F' U'", "L' U L U"          # insert moves (second layer)
+                "U", "U'"
+            ])
+        elif self.phase == Phase.OLL:
+            pass
+        elif self.phase == Phase.PLL:
+            pass
+        else:
+            raise Exception(f"Unrecognized {self.phase} phase")
         self._colors_to_positions = {"U": "W", "D": "Y", "F": "G", "R": "R", "B": "B", "L": "O"}
-        self.action_space = np.array(["U", "D", "F", "R", "B", "L","U'", "D'", "F'", "R'", "B'", "L'"])
 
         self._piece_mapper = {
             0:  (1, 3, 4), 1:  (1, 3, -1), 2:  (1, 3, 5), 3:  (1, -1, 4), 4:  (1, -1, -1), 5:  (1, -1, 5),
@@ -94,16 +119,29 @@ class Environment:
         left = all([face == "L" for face in faces[4]])
         back = all([face == "B" for face in faces[5]])
 
-        return self.algorithm.reward() == 100
+        rewward = self.algorithm.reward(
+            lbl_phase="cross" if self.phase == Phase.CROSS else
+                      "f2l" if self.phase == Phase.F2L else
+                      None
+        )
+
+        return rewward == 100
         #return top and right and front and bottom and left and back
     
     def scramble(self) -> None:
         '''
         Scrambles the cube
         '''
-        self.cube.rotate(' '.join(random.choices(self.action_space, k=self._scramble_moves)))
-        self.state = np.array(list(self.cube.get_kociemba_facelet_positions()))
-        self.state2 = self._get_state()
+        if self.phase == Phase.CROSS:
+            self.cube.rotate(' '.join(random.choices(self.action_space, k=self._scramble_moves)))
+            self.state = np.array(list(self.cube.get_kociemba_facelet_positions()))
+            self.state2 = self._get_state()
+        elif self.phase == Phase.F2L:
+            pass
+        elif self.phase == Phase.OLL:
+            pass
+        elif self.phase == Phase.PLL:
+            pass
 
     def step(
         self,
@@ -112,13 +150,12 @@ class Environment:
         # makes action
         if action not in self.action_space:
             raise ValueError(f"Unrecognized '{action}' move")
-        
-        # update state
+
         self.cube.rotate(action)
         self.state = np.array(list(self.cube.get_kociemba_facelet_positions()))
         self.state2 = self._get_state()
 
         # calculate reward
-        reward = self.algorithm.reward()
+        reward = self.algorithm.reward(lbl_phase=self.phase)
 
         return (self.state, reward, self.is_terminated())
