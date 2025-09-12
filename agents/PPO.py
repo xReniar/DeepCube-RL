7439a9ad1e6e9fefdd522d7ca7809323c629899a
xReniar <ilaorainer@gmail.com>
2025-09-12 23:36:40 +0200

    Modify input dim netwotk from 5 to 8

--- agents/PPO.py
+++ agents/PPO.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from .agent import Agent
-from environment import Environment
 from torch.distributions import MultivariateNormal
 import numpy as np
 
@@ -52,10 +51,12 @@ class Network(nn.Module):
 class PPO(Agent):
     def __init__(
         self,
-        env: Environment,
+        env,
+        phase: str,
         args: dict
     ) -> None:
-        super().__init__(env, args)
+        super().__init__(env, phase)
+        args = args["PPO"][phase]
         self.total_timesteps = int(args["total_timesteps"])
         self.timesteps_per_batch = int(args["timesteps_per_batch"])
         self.max_timesteps_per_episode = int(args["max_timesteps_per_episode"])
@@ -69,8 +70,8 @@ class PPO(Agent):
         self.act_dim = self.env.action_space.shape[0]
 
         # initialize actor and critic networks)
-        self.actor = Network(5, 128, self.act_dim).to(self.device)
-        self.critic = Network(5, 128, 1).to(self.device)
+        self.actor = Network(8, 128, self.act_dim).to(self.device)
+        self.critic = Network(8, 128, 1).to(self.device)
         self.actor_optim = optim.Adam(self.actor.parameters(), lr=self.lr)
         self.critic_optim = optim.Adam(self.critic.parameters(), lr=self.lr)
 
@@ -114,6 +115,8 @@ class PPO(Agent):
                 action, log_prob = self.action(self.state_to_tensor(obs))
                 obs, reward, done = self.env.step(self.action_to_move(action.argmax().item()))
                 obs = self.env.state2
+
+                print(self.action_to_move(action.argmax().item()), reward, done)
 
                 ep_rews.append(reward)
                 batch_acts.append(action)
