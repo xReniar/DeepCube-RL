72443c793e90b6f075e43ba49026a167dd0b240f
xReniar <ilaorainer@gmail.com>
2025-09-16 00:25:04 +0200

    Create move swapper for f2l

---
+++ utils/move_swapper.py
@@ -0,0 +1,44 @@
+from magiccube import Cube
+import json
+import time
+
+
+def mov(moveset: list, c: int):
+    moves = ["F", "R", "B", "L"]
+    copy = moves
+    for _ in range(c):
+        first = copy[0]
+        other = copy[1:]
+        other.append(first)
+
+        copy = other
+
+    new_moveset = []
+    for m in moveset:
+        if m == "F":
+            new_moveset.append(copy[0])
+        elif m == "F'":
+            new_moveset.append(f"{copy[0]}'")
+        elif m == "R":
+            new_moveset.append(copy[1])
+        elif m == "R'":
+            new_moveset.append(f"{copy[1]}'")
+        elif m == "B":
+            new_moveset.append(copy[2])
+        elif m == "B'":
+            new_moveset.append(f"{copy[2]}'")
+        elif m == "L":
+            new_moveset.append(copy[3])
+        elif m == "L'":
+            new_moveset.append(f"{copy[3]}'")
+        else:
+            new_moveset.append(m)
+
+    return new_moveset
+
+
+cube = Cube()
+
+moves = "F' U F U"
+for i in range(1, 4):
+    print(" ".join(mov(moves.split(), i)))
